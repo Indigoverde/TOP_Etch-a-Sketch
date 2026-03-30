46efddd47c9995fd687688e4156141561112fbf2
Indigoverde <207912346+Indigoverde@users.noreply.github.com>
2026-03-30 20:09:41 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Grid
+const container = document.querySelector("#gridContainer");
+
+const div = document.createElement("div");
+div.style.border = "solid blue"
+div.style.minHeight = " 50 px";
+div.style.minWidth = "50px"
+container.appendChild(div);
+
+// voir avec flexbox pour une taille auto
